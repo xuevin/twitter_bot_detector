ca8ef91ab8eb80f75065314ed75785f6112fa928
Vincent Xue <xue.vin@gmail.com>
2018-04-25 20:39:53 +0000

    Got ajax to work - moving on to graph

--- app.py
+++ app.py
@@ -1,14 +1,98 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, jsonify, url_for
+from celery import Celery
+import random 
+import os
+import time
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+@app.route('/scoreUser',methods=['GET','POST'])
+def scoreUser():
+    if request.method == 'GET':
+        return "oops"
+    else:
+        pass
+        #request.form['twitterUserName']
+        #executeScript = "<script> chart.load({columns: [['data', 22]]}); </script>"
+        #return render_template('index.html',scoreUser=executeScript)
+
+
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking'] 
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+   
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+	task = long_task.apply_async()
+	return jsonify({}), 202, {'Location': url_for('taskstatus', task_id=task.id)}
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
+
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(host='0.0.0.0',port=33507,debug=1)
